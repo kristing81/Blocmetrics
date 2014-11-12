338d8518e12e2065b6a1cb0a74478c4aae19c57f
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-11-12 19:32:27 +0800

    [FIX]

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,10 +3,13 @@ class Api::V1::EventsController < Api::V1::BaseController
   def create
     @tracked_domain = TrackedDomain.find(params[:verification_code])
     # find a domain via the submitted verification token
-    @event = Event.new(event_params).to_json
+    @event = @tracked_domain.event.new(event_params).to_json
     if @event.save
     # record event
     render nothing: true, status:200
+    else 
+      render :new
+    end
   end
 
   private
@@ -15,3 +18,4 @@ class Api::V1::EventsController < Api::V1::BaseController
     params.require(:event).permit(:tracked_domain_id, :event_type, :url, :ip_address, :created_at, :updated_at) 
   end
 end
+ 
