7132480f1ce4a8449238669a83e09b04875eb3ec
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-11-12 19:45:30 +0800

    [FIX]

---
+++ public/agent.js
@@ -0,0 +1,19 @@
+var blocmetrics = (function (tracked_domain_id, event_type) {
+    var _bm_event = {
+        tracked_domain_id: tracked_domain_id,
+        event_type: event_type
+    }
+
+    var _bm_request = new XMLHttpRequest();
+    _bm_request.open("POST", "http://localhost:4000.com/events.json", true);
+    _bm_request.setRequestHeader('Content-Type', 'application/json');
+    _bm_request.onreadystatechange = function () {
+
+    };
+
+
+    _bm_request.send(JSON.stringify(_bm_event));
+}());
+
+
+// = javascript_include_tag "http://kristingonzalez-blocmetrics.herokuapp.com/api/v1/agent.js"
