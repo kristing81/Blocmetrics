97091c4f22dcc0a888d437e005f27d1ccc0abdcc
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-11-17 17:01:32 +0800

    verifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  data_table = GoogleVisualr::DataTable.new
  protected
 
  def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :events, only: [:create, :index]
+      match 'events' => "events#index", via: :options
+      resources :events, only: [:create, :index, :show]
     end
   end
   
