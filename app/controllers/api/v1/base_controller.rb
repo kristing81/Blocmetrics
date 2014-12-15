176e3c64d5ec00ce111b1e15b077d6b6fc3c490a
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-12-15 19:22:59 +0800

    amendign authentication method

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,23 +9,12 @@ class Api::V1::BaseController < ApplicationController
   # do not run those 2 if the request.method is :options
   before_filter :authenticate_user_from_token!
 
-  # def authenticate_user_from_token!
-  #   token = params[:authentication_token]
-  #   user = User.where(authentication_token: token).first
-  #   if user
-  #     sign_in(user, store: false)
-  #   else
-  #     render nothing: true, status: :unauthorized
-  #   end
-  # end
-  def authenticate_user_from_token!
-    user_email = params[:user_email].presence
-    user       = user_email && User.find_by_email(user_email)
-
-    # Notice how we use Devise.secure_compare to compare the token
-    # in the database with the token given in the params, mitigating
-    # timing attacks.
-    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+   def authenticate_from_user_token!
+    token = params[:auth_token]
+    user = User.where(authentication_token: token).first
+ 
+    logger.info user.inspect
+    if user
       sign_in user, store: false
     end
   end
